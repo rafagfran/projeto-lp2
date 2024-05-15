b0403dc29bc89471d6f02871b00679afc3c9e5a9
Rafael <Rafaelgfra@hotmail.com>
2024-05-15 00:03:51 -0300

    select de paginação ajueste de tamanho

--- src/components/common/SelectCommon.jsx
+++ src/components/common/SelectCommon.jsx
@@ -7,9 +7,11 @@ const SelectCommon = ({id, defaultValue, onchangeSet, textLabel, options}) => {
     display: 'flex',
     flexDirection: 'column',
   }
+
+
   return (
     <div style={styleSelect}>
-        <label htmlFor={id}>{textLabel}</label>
+        <label  htmlFor={id}>{textLabel}</label>
         <select name={id} id={id} defaultValue={defaultValue} onChange={(e) => {onchangeSet(e.target.value)}}>
             {options.map(option => (
                 <option key={option.value} value={option.value}>{option.text}</option>
